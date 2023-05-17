822b2245b6444edcba9ee7a64fd8f717f9dc5d57
Claes Lundin <claeslundin@pm.me>
2023-05-17 14:44:04 +0200

    box with 2 textures

--- learn-opengl/Program.cpp
+++ learn-opengl/Program.cpp
@@ -2,12 +2,19 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include "Shaders.h"
+#include "stb_image.h"
+#include <glm/glm/glm.hpp>
+#include <glm/glm/gtc/matrix_transform.hpp>
+#include <glm/glm/gtc/type_ptr.hpp>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-void KeyCallbackToggleLines(GLFWwindow* window, int key, int scancode, int action, int mods);
+void KeyCallbacks(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 static bool drawLines = false;
+static float xOffset = 0.0f;
+static float yOffset = 0.0f;
+static float blendScale = 0.2f;
 
 int main() {
 
@@ -55,15 +62,21 @@ int main() {
 		return -1;
 	}
 
+
 	// Reads text from files and compiles the shader program from that
 	Shader ourShader("VertexShader.txt", "FragmentShader.txt");
 
 
 	float vertices[] = {
-		// positions         // colors
-		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+	};
+	unsigned int indices[] = {
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
 	};
 
 	
@@ -76,8 +89,9 @@ int main() {
 	// Multiples of these are in an array and not seperate
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
-	unsigned int VBO;
+	unsigned int VBO, EBO;
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 
 	// Initialization code (done once(unless the object frequently changes))
@@ -85,18 +99,115 @@ int main() {
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// glVertexAttribPointer arguments
+	// Location = 0 so we pass in 0
+	// We use vec3 so we pass in 3 values
+	// We specifies the data type as float
+	// If we want nomalaized data to be used we use GL_TRUE
+	// Fifth argument is the "stride" it's the distance from one point to another in bytes
+	// The last parameter is the offset off where another stride (for color for example) begins
+	
 	// Position attributes
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	// Color attributes
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3* sizeof(float)));
 	glEnableVertexAttribArray(1);
-
+	// Texture attributes
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
 	// Press 'L' to change from Line or Fill triangles
 	// Sets the keycallback we created to a specific window
 	// This is used if we a key press only do someting once per click
-	glfwSetKeyCallback(window, KeyCallbackToggleLines);
+	glfwSetKeyCallback(window, KeyCallbacks);
+
+
+	// Texture 1
+	// Generates texture and stores it in a unasinged int 
+	unsigned int texture1, texture2;
+	glGenTextures(1, &texture1);
+
+	// Binds the texture to the active texture unit
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+
+	// Set the texture wrapping/filtering options (on the currently bound texture object)
+	// 
+	// Wrapping options:
+	// GL_REPEAT repeats image
+	// GL_MIRRORED_REPEAT self explainatory
+	// GL_CLAMP_TO_EDGE drags the edge out
+	// GL_CMALP_TO_BORDER gives the image a border (needs an exta parameter input for border color)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	// Filtering options:
+	// GL_LINEAR smooth 
+	// GL_NEARES pixelated
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// Loads the image from a path and stores it as a char
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* data = stbi_load("Textures/WoodenContainer.jpg", &width, &height, &nrChannels, 0);
+	
+
+	if (data)
+	{
+		// Arguments for glTexImage2D
+		// Specify texture target
+		// Specify the Mipmap level
+		// The format we want to the texture as
+		// Sets the width
+		// Sets the height
+		// Always 0 (legacy stuff)
+		// Specify the format of the source img
+		// Specify the datatype of the source img
+		// The actual image data
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	// Frees the imagedata from memory
+	// Use after done loading the image
+	stbi_image_free(data);
+
+	// Texture 2
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	data = stbi_load("Textures/awesomeface.png", &width, &height, &nrChannels, 0);
+
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	// tells each uniform sampler in the fragment shader which texture unit they belong to (only has to be done once hence why it is out of the render loop)  
+	ourShader.use();
+	// manualy like this 
+	glUniform1i(glGetUniformLocation(ourShader.ID, "ourTexture1"), 0);
+	// or via the texture class
+	ourShader.setInt("ourTexture2", 1);
+
 
 	// Checks if GLFW has been instructed to close (this is the render loop)
 	while (!glfwWindowShouldClose(window))
@@ -113,10 +224,29 @@ int main() {
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// Activates a texture unit. we can have up to 16 per shader GL_TEXTURE0 - 15
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+
 		ourShader.use();
+
+		//glUniform1i(glGetUniformLocation(ourShader.ID, "ourTexture1"), 0);
+		//ourShader.setInt("ourTexture1", 1);
+
+		// moves the object on the screen
+		ourShader.setFloat("xOffset", xOffset);
+		ourShader.setFloat("yOffset", yOffset);
+		ourShader.setFloat("blendScale", blendScale);
+
+
 		glBindVertexArray(VAO);
 		// Draws the first triangle from the latest binded VAO
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// Draws the elements from EBO
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
 
 		// Check and call events and swap the buffers below here
@@ -134,6 +264,7 @@ int main() {
 	// optional: de-allocate all resources once they've outlived their purpose
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	// Cleans up all the resources used and properly exits the application
 	glfwTerminate();
@@ -154,10 +285,45 @@ void processInput(GLFWwindow* window) {
 	}
 }
 
-
 // A callback function that registers a key press and does something after that
-void KeyCallbackToggleLines(GLFWwindow* window, int key, int scancode, int action, int mods) 
+void KeyCallbacks(GLFWwindow* window, int key, int scancode, int action, int mods) 
 {
+	if (action == GLFW_RELEASE)
+	{
+		return;
+	}
+
+	switch (key)
+	{
+	case GLFW_KEY_DOWN:
+		yOffset -= 0.1;
+		break;
+
+	case GLFW_KEY_UP:
+		yOffset += 0.1;
+		break;
+
+	case GLFW_KEY_LEFT:
+		xOffset -= 0.1;
+		break;
+
+	case GLFW_KEY_RIGHT:
+		xOffset += 0.1;
+		break;
+
+	case GLFW_KEY_M:
+		blendScale += 0.1;
+		break;
+
+	case GLFW_KEY_N:
+		blendScale -= 0.1;
+		break;
+
+		default:
+			break;
+	}
+
+
 	// Click Toggle
 	//if (key == GLFW_KEY_L && action == GLFW_PRESS)
 	//{
